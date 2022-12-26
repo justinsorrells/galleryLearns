e7746561dc2d43ba2a4bc1c9c64ceff8cfdb9ed9
Justin Sorrells <justinwsorrells@gmail.com>
2022-12-26 10:51:58 -0500

    Final Project

--- galleryLearns/controllers/indexController.js
+++ galleryLearns/controllers/indexController.js
@@ -46,7 +46,6 @@ exports.index_post_get = function(req, res, next) {
                     title: "Unable to access post",
                 })
             }
-            console.log(result.comments);
             res.render('post', {
                 result: result.module,
                 title: result.module.title,
@@ -55,63 +54,105 @@ exports.index_post_get = function(req, res, next) {
             })
         }
     )
-    // modules.findById({ _id: req.params.id }).exec((err, result) => {
-    //     if (err) {
-    //         return next(err);
-    //     }
-
-    //     if (!result) {
-    //         res.render('index', {
-    //             title: "Unable to access post :(",
-    //         });
-    //         return;
-    //     }
-
-    //     res.render('post', {
-    //         result: result,
-    //         title: result.title,
-    //         postContent: marked.parse(result.content)
-    //     })
-    // })
 }
 
-exports.comment_post = function(req, res, next) {
-    async.parallel(
-        {
-            post(callback) {
-                modules.findById({ _id: req.params.id }).exec(callback);
-            },
-            profile(callback) {
-                user.findOne({ cookie: req.session.id }).exec(callback);
-            }
-        },
-        (err, result) => {
-            if (err) {
-                return next(err);
-            }
+exports.comment_post = [
 
-            if (!result) {
-                res.redirect("error", {
-                    message: "Error retrieving info from database",
-                })
-                return;
-            }
-            if (result.post._id && result.profile._id) {
-                const userComment = new comment({
+    body("newComment")
+        .trim()
+        .escape()
+        .isLength({ min: 1 })
+        .withMessage("Please enter a comment"),
+
+    (req, res, next) => {
+
+        const errors = validationResult(req);
+        console.log(`New Comment: ${req.body.newComment}`)
+        if (!errors.isEmpty()) {
+            req.session.errors = errors.array();
+            res.redirect('/posts/' + req.params.id + "?e=" + encodeURIComponent("Cannot leave a blank comment"));
+            return;
+        }
+
+        async.parallel(
+            {
+                post(callback) {
+                    modules.findById({ _id: req.params.id }).exec(callback);
+                },
+                profile(callback) {
+                    user.findOne({ cookie: req.session.id }).exec(callback);
+                }
+            },
+            (err, result) => {
+                if (err) {
+                    return next(err);
+                }
+
+                if (!result) {
+                    res.redirect("error", {
+                        message: "Error retrieving info from database",
+                    })
+                    return;
+                }
+                if (result.post._id && result.profile._id) {
+                    const userComment = new comment({
                     post: result.post._id,
                     user: result.profile._id, 
                     comment: req.body.newComment,
-                })
-                userComment.save((err) => {
-                    if (err) {
-                        return next(err);
-                    }
-                    res.redirect('/posts/' + result.post._id);
-                    return;
-                })
-            } else {
-                res.redirect('/');
+                    })
+                    userComment.save((err) => {
+                        if (err) {
+                            return next(err);
+                        }
+                        res.redirect('/posts/' + result.post._id);
+                        return;
+                    })
+                } else {
+                    res.redirect('/');
+                }
             }
-        }
-    )
-}
+        )
+    }
+]
+
+
+// exports.comment_post = function(req, res, next) {
+//     async.parallel(
+//         {
+//             post(callback) {
+//                 modules.findById({ _id: req.params.id }).exec(callback);
+//             },
+//             profile(callback) {
+//                 user.findOne({ cookie: req.session.id }).exec(callback);
+//             }
+//         },
+//         (err, result) => {
+//             if (err) {
+//                 return next(err);
+//             }
+
+//             if (!result) {
+//                 res.redirect("error", {
+//                     message: "Error retrieving info from database",
+//                 })
+//                 return;
+//             }
+//             if (result.post._id && result.profile._id) {
+//                 const userComment = new comment({
+//                     post: result.post._id,
+//                     user: result.profile._id, 
+//                     comment: req.body.newComment,
+//                 })
+//                 userComment.save((err) => {
+//                     if (err) {
+//                         return next(err);
+//                     }
+//                     res.redirect('/posts/' + result.post._id);
+//                     return;
+//                 })
+//             } else {
+//                 res.redirect('/');
+//             }
+//         }
+//     )
+// }
