b84f7114e7b492a4057e965cc629473fa6d39649
justinsorrells <108559580+justinsorrells@users.noreply.github.com>
2023-01-08 15:12:18 -0500

    Update markdownPreviewer.js

--- galleryLearns/public/javascript/markdownPreviewer.js
+++ galleryLearns/public/javascript/markdownPreviewer.js
@@ -7,10 +7,6 @@ marked.setOptions({
     breaks: true
 })
 
-console.log(marked);
-console.log(input);
-console.log(output);
-
 window.addEventListener('DOMContentLoaded', function() {
     input.addEventListener('change', function(e) {
         output.innerHTML = marked.parse(e.target.value);
